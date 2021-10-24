bdb157c4c5e6dd98381be622b39955d9829a5130
Aryan Karan <aryankaran28022004@gmail.com>
2021-10-24 09:57:45 +0530

    Download arm32 gcc too

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -8,6 +8,7 @@ function setup () {
     
     # Download gcc
     git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r47 --depth=1 gcc
+    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r47 --depth=1 gcc-32
 
     # Download Clang
     wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-11.0.0_r37/clang-r365631c.tar.gz
