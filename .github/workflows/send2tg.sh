baf09c5b3100ad3ea2d44c97084a43f909d0ae45
Aryan Karan <aryankaran28022004@gmail.com>
2021-10-24 22:20:31 +0530

    Update export

--- .github/workflows/send2tg.sh
+++ .github/workflows/send2tg.sh
@@ -26,7 +26,7 @@ function send_zip() {
     
     # Send the zip to telegram through bot
     curl "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument?chat_id=${CHAT_ID}" \
-    -F document=@$ZIP -F caption="✔ md5 : <code>`echo -e "\n$(md5sum $ZIP | cut -d " " -f1)\n\n🔷 "`</code> sha256 : <code>`echo -e "\n$(md5sum $ZIP | cut -d " " -f1)\n\n💥"`</code> Repo Link: $repo `echo -e "\n\n🧲"` Branch Name: <code>$branch </code> `echo -e "\n\n👑"` Kernel Version: <code>$kversion </code> `echo -e "\n\n💦"` defconfig: <code>$DEFCONFIG </code>" -F "parse_mode=HTML" \
+    -F document=@$ZIP -F caption="✔ md5 : <code>`echo -e "\n$(md5sum $ZIP | cut -d " " -f1)\n\n🔷 "`</code> sha256 : <code>`echo -e "\n$(md5sum $ZIP | cut -d " " -f1)\n\n💥"`</code> Repo Link: $repo_link `echo -e "\n\n🧲"` Branch Name: <code>$branch </code> `echo -e "\n\n👑"` Kernel Version: <code>$kversion </code> `echo -e "\n\n💦"` defconfig: <code>$DEFCONFIG </code>" -F "parse_mode=HTML" \
     -F thumb=@image
 }
 
